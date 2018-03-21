062559389e21d851fe8bc349081b6c32b3c6dbf8
zern <jiaozi362330@gmail.com>
2018-03-21 23:49:27 +0800

    client test

--- src/main/java/cn/izern/test/zookeeper/Main.java
+++ src/main/java/cn/izern/test/zookeeper/Main.java
@@ -8,24 +8,34 @@ import org.apache.curator.x.discovery.ServiceDiscovery;
 import org.apache.curator.x.discovery.ServiceInstance;
 
 import cn.izern.test.zookeeper.server.Server1;
+import cn.izern.test.zookeeper.server.Server2;
 import cn.izern.test.zookeeper.util.ZookeeperUtils;
 
 public class Main {
 
 	
 	public static void main(String[] args) throws Exception {
+		
+		// start server 
 		for (int i = 0; i < 5; i++) {
 			Runnable run = new Server1();
 			Thread thread = new Thread(run);
 			thread.setName("thread-"+ i);
 			thread.start();
 		}
+		for (int i = 0; i < 5; i++) {
+			Runnable run = new Server2();
+			Thread thread = new Thread(run);
+			thread.setName("thread2-"+ i);
+			thread.start();
+		}
 		int i = 0;
 		CuratorFramework client = ZookeeperUtils.getClient();
 		ServiceDiscovery<String> discovery = ZookeeperUtils.getDiscovery(client);
-		while(i < 20) {
+		while(i < 100) {
 			Collection<ServiceInstance<String>> instances = discovery.queryForInstances("server1");
-			System.out.println("--------- find instance --------");
+			System.out.println("--------- find server1 instance --------");
+			System.out.println("instance count " + instances.size());
 			instances.forEach((instance)->{
 				System.out.println(instance.getId());
 			});
---
+++ src/main/java/cn/izern/test/zookeeper/client/Client1.java
@@ -0,0 +1,16 @@
+package cn.izern.test.zookeeper.client;
+
+/**
+ * client
+ * @author root
+ * 2018年3月21日 下午11:47:15
+ */
+public class Client1 implements Runnable{
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
